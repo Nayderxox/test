6952ce92aa9cb39dd00183128ebd0fb347dafbd1
Nayderxox <Nadirfre10@gmail.com>
2024-12-15 01:25:13 +0000

    new

--- main.java
+++ main.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class main {
-	public static void main(String[] args) {
-		System.out.println("check"); }
+	public static void main(String[] args) 
+	{ 	
+
+		Scanner keyboard = new Scanner(System.in);	
+		System.out.println("What is your name?");
+		String x = keyboard.nextLine();
+
+
+		System.out.println("Everything seems to be working fine " + x);
+
+
+ }
 }
